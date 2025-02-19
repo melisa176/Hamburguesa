958991f710eca800b3d74ea99c037240886f4e05
melisa176 <carolinacastillon27@gmail.com>
2025-02-18 23:16:37 -0500

    hola

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,17 +1,25 @@
 const contenedorTarjetas = document.getElementById("productos-container");
 
-/** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
+/** Crea las tarjetas de productos teniendo en cuenta la lista en comida.js */
 function crearTarjetasProductosInicio(productos){
   productos.forEach(producto => {
     const nuevaComida = document.createElement("div");
-    nuevaComida.classList = "tarjeta-producto"
+    nuevaComida.classList = "tarjeta-producto";
     nuevaComida.innerHTML = `
-    <img src="./img/${producto.id}.png">
-    <h3>${producto.nombre}</h3>
-    <p class="precio">$${producto.precio}</p>
-    <button>Agregar al carrito</button>`
+      <div class="tarjeta-contenido">
+        <img src="./img/${producto.id}.png" alt="${producto.nombre}" class="imagen-producto">
+        <div class="informacion-producto">
+          <h3>${producto.nombre}</h3>
+          <p class="descripcion">${producto.descripcion}</p>
+          <p class="precio">S/ ${producto.precio}</p>
+          <button>Agregar al pedido</button>
+        </div>
+      </div>
+    `;
+    
     contenedorTarjetas.appendChild(nuevaComida);
-    nuevaComida.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+    nuevaComida.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto));
   });
 }
+
 crearTarjetasProductosInicio(comida);
